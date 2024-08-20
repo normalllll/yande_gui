601f202db5b59785afb7fcd4925aa18dd98f3941
normalllll <147307373+normalllll@users.noreply.github.com>
2024-08-20 08:47:48 +0000

    Clear all

--- lib/pages/post_list/post_list_page.dart
+++ lib/pages/post_list/post_list_page.dart
@@ -4,7 +4,6 @@ import 'package:flutter/services.dart';
 import 'package:flutter_animate/flutter_animate.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:loading_more_list/loading_more_list.dart';
-import 'package:pull_to_refresh_notification/pull_to_refresh_notification.dart';
 import 'package:yande_gui/components/loading_more_indicator/loading_more_indicator.dart';
 import 'package:yande_gui/components/yande_image/yande_image.dart';
 import 'package:yande_gui/enums.dart';
@@ -26,22 +25,6 @@ class PostListPage extends ConsumerStatefulWidget {
 
 class _PostListPageState extends ConsumerState<PostListPage> {
   late final provider = postListProvider(runtimeType, tags: widget.tags ?? []);
-
-  Widget buildPullToRefreshHeader(PullToRefreshScrollNotificationInfo? info) {
-    return SliverToBoxAdapter(
-      child: switch (info?.mode) {
-        null => const SizedBox(),
-        PullToRefreshIndicatorMode.drag => const Icon(Icons.arrow_downward_outlined),
-        PullToRefreshIndicatorMode.armed => const Icon(Icons.arrow_upward_outlined),
-        PullToRefreshIndicatorMode.snap => const Icon(Icons.arrow_upward_outlined),
-        PullToRefreshIndicatorMode.refresh => const CupertinoActivityIndicator(),
-        PullToRefreshIndicatorMode.done => const Icon(Icons.check),
-        PullToRefreshIndicatorMode.canceled => const Icon(Icons.close_outlined),
-        PullToRefreshIndicatorMode.error => const Icon(Icons.error_outline_outlined),
-      },
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
     final state = ref.watch(provider);
@@ -57,111 +40,108 @@ class _PostListPageState extends ConsumerState<PostListPage> {
         return Column(
           children: [
             Expanded(
-              child: PullToRefreshNotification(
-                maxDragOffset: 30,
-                pullBackOnRefresh: true,
-                onRefresh: () async {
-                  await HapticFeedback.mediumImpact();
-                  return state.source.refresh(true);
-                },
-                child: LoadingMoreCustomScrollView(
-                  slivers: [
-                    SliverAppBar(
-                      floating: true,
-                      snap: true,
-                      scrolledUnderElevation: 0,
-                      title: switch (widget.tags) { final tags? => Text(i18n.postList.titleWithTags(tags.join(' '))), _ => Text(i18n.postList.title) },
-                    ),
-                    PullToRefreshContainer(buildPullToRefreshHeader),
-                    LoadingMoreSliverList(
-                      SliverListConfig(
-                        extendedListDelegate: SliverWaterfallFlowDelegateWithFixedCrossAxisCount(crossAxisCount: SettingsService.waterfallColumns ?? columnsMax),
-                        itemBuilder: (BuildContext context, item, int index) {
-                          return GestureDetector(
-                            onTap: () {
-                              Navigator.of(context).push(MaterialPageRoute(builder: (context) => PostDetailPage(post: item)));
-                            },
-                            child: LayoutBuilder(
-                              builder: (context, constraints) {
-                                const padding = 4;
-                                final width = constraints.maxWidth - padding * 2;
-                                final height = (item.height * width / item.width) - padding * 2;
-                                return Padding(
-                                  padding: const EdgeInsets.all(4),
-                                  child: Stack(
-                                    children: [
-                                      ClipRRect(
-                                        borderRadius: BorderRadius.circular(6),
-                                        child: YandeImage(
-                                          item.previewUrl,
-                                          width: width,
-                                          height: height,
-                                          imageBuilder: (child) {
-                                            return Hero(
-                                              tag: item.id,
-                                              child: child,
-                                            ).animate().fade(duration: 250.ms);
-                                          },
-                                        ),
+              child: LoadingMoreCustomScrollView(
+                slivers: [
+                  SliverAppBar(
+                    floating: true,
+                    snap: true,
+                    scrolledUnderElevation: 0,
+                    title: switch (widget.tags) { final tags? => Text(i18n.postList.titleWithTags(tags.join(' '))), _ => Text(i18n.postList.title) },
+                  ),
+                  CupertinoSliverRefreshControl(
+                    onRefresh: () async {
+                      await HapticFeedback.mediumImpact();
+                      await state.source.refresh();
+                    },
+                  ),
+                  LoadingMoreSliverList(
+                    SliverListConfig(
+                      extendedListDelegate: SliverWaterfallFlowDelegateWithFixedCrossAxisCount(crossAxisCount: SettingsService.waterfallColumns ?? columnsMax),
+                      itemBuilder: (BuildContext context, item, int index) {
+                        return GestureDetector(
+                          onTap: () {
+                            Navigator.of(context).push(MaterialPageRoute(builder: (context) => PostDetailPage(post: item)));
+                          },
+                          child: LayoutBuilder(
+                            builder: (context, constraints) {
+                              const padding = 4;
+                              final width = constraints.maxWidth - padding * 2;
+                              final height = (item.height * width / item.width) - padding * 2;
+                              return Padding(
+                                padding: const EdgeInsets.all(4),
+                                child: Stack(
+                                  children: [
+                                    ClipRRect(
+                                      borderRadius: BorderRadius.circular(6),
+                                      child: YandeImage(
+                                        item.previewUrl,
+                                        width: width,
+                                        height: height,
+                                        imageBuilder: (child) {
+                                          return Hero(
+                                            tag: item.id,
+                                            child: child,
+                                          ).animate().fade(duration: 250.ms);
+                                        },
                                       ),
-                                      if (item.parentId != null)
-                                        Positioned(
-                                          right: 0,
-                                          top: 0,
-                                          child: Container(
-                                            padding: const EdgeInsets.all(2),
-                                            decoration: BoxDecoration(
-                                              color: Colors.black.withOpacity(0.5),
-                                            ),
-                                            child: const Icon(Icons.more_outlined, color: Colors.white),
-                                          ),
-                                        )
-                                      else if (item.hasChildren)
-                                        Positioned(
-                                          right: 0,
-                                          top: 0,
-                                          child: Container(
-                                            padding: const EdgeInsets.all(2),
-                                            decoration: BoxDecoration(
-                                              color: Colors.black.withOpacity(0.5),
-                                            ),
-                                            child: const Icon(Icons.more_horiz, color: Colors.white),
-                                          ),
-                                        ),
+                                    ),
+                                    if (item.parentId != null)
                                       Positioned(
                                         right: 0,
-                                        bottom: 0,
+                                        top: 0,
                                         child: Container(
                                           padding: const EdgeInsets.all(2),
                                           decoration: BoxDecoration(
                                             color: Colors.black.withOpacity(0.5),
                                           ),
-                                          child: Text(
-                                            '${Resolution.match(item.width * item.height).title} ${item.width} x ${item.height}',
-                                            style: const TextStyle(color: Colors.white),
+                                          child: const Icon(Icons.more_outlined, color: Colors.white),
+                                        ),
+                                      )
+                                    else if (item.hasChildren)
+                                      Positioned(
+                                        right: 0,
+                                        top: 0,
+                                        child: Container(
+                                          padding: const EdgeInsets.all(2),
+                                          decoration: BoxDecoration(
+                                            color: Colors.black.withOpacity(0.5),
                                           ),
+                                          child: const Icon(Icons.more_horiz, color: Colors.white),
                                         ),
                                       ),
-                                    ],
-                                  ),
-                                );
-                              },
-                            ),
-                          );
+                                    Positioned(
+                                      right: 0,
+                                      bottom: 0,
+                                      child: Container(
+                                        padding: const EdgeInsets.all(2),
+                                        decoration: BoxDecoration(
+                                          color: Colors.black.withOpacity(0.5),
+                                        ),
+                                        child: Text(
+                                          '${Resolution.match(item.width * item.height).title} ${item.width} x ${item.height}',
+                                          style: const TextStyle(color: Colors.white),
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              );
+                            },
+                          ),
+                        );
+                      },
+                      sourceList: state.source,
+                      padding: EdgeInsets.zero,
+                      indicatorBuilder: (context, status) => LoadingMoreIndicator(
+                        status: status,
+                        isSliver: true,
+                        errorRefresh: () {
+                          state.source.errorRefresh();
                         },
-                        sourceList: state.source,
-                        padding: EdgeInsets.zero,
-                        indicatorBuilder: (context, status) => LoadingMoreIndicator(
-                          status: status,
-                          isSliver: true,
-                          errorRefresh: () {
-                            state.source.errorRefresh();
-                          },
-                        ),
                       ),
                     ),
-                  ],
-                ),
+                  ),
+                ],
               ),
             ),
           ],
