0969d26e0fdd236e604754b256f96c40968c71d3
normalllll <147307373+normalllll@users.noreply.github.com>
2024-10-19 18:27:56 +0000

    Add preview tag for download tasks

--- lib/pages/downloads/download_task.dart
+++ lib/pages/downloads/download_task.dart
@@ -4,6 +4,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:yande_gui/components/yande_image/yande_image.dart';
 import 'package:yande_gui/pages/downloads/logic.dart';
 import 'package:yande_gui/pages/post_detail/post_detail_page.dart';
+import 'package:yande_gui/widgets/tag/tag.dart';
 
 class DownloadTaskWidget extends ConsumerWidget {
   final DownloadTaskProvider provider;
@@ -28,45 +29,65 @@ class DownloadTaskWidget extends ConsumerWidget {
               YandeImage(provider.post.previewUrl, width: 150, height: 150),
               const SizedBox(width: 20),
               Expanded(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Row(
-                      children: [
-                        Text(
-                          'ID:${provider.post.id}',
-                          style: const TextStyle(fontSize: 20),
+                child: SizedBox(
+                  height: 150,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        children: [
+                          Text(
+                            'ID:${provider.post.id}',
+                            style: const TextStyle(fontSize: 20),
+                          ),
+                          const SizedBox(width: 20),
+                          switch (state.type) {
+                            DownloadTaskStateType.idle => GestureDetector(
+                                behavior: HitTestBehavior.opaque,
+                                onTap: () {
+                                  ref.read(provider.notifier).doDownload();
+                                },
+                                child: const Icon(Icons.download_outlined),
+                              ),
+                            DownloadTaskStateType.busy => const CupertinoActivityIndicator(),
+                            DownloadTaskStateType.completed => GestureDetector(
+                                behavior: HitTestBehavior.opaque,
+                                onTap: () {
+                                  ref.read(provider.notifier).doDownload(retry: true);
+                                },
+                                child: const Icon(Icons.check_outlined),
+                              ),
+                            DownloadTaskStateType.failed => GestureDetector(
+                                behavior: HitTestBehavior.opaque,
+                                onTap: () {
+                                  ref.read(provider.notifier).doDownload(retry: true);
+                                },
+                                child: const Icon(Icons.error_outline),
+                              ),
+                          }
+                        ],
+                      ),
+                      const SizedBox(height: 10),
+                      LinearProgressIndicator(value: state.progress),
+                      Expanded(
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 8),
+                          child: ClipRect(
+                            child: Wrap(
+                              runSpacing: 14,
+                              spacing: 6,
+                              children: [
+                                for (final tag in provider.post.tags.split(' '))
+                                  TagWidget(
+                                    text: tag,
+                                  ),
+                              ],
+                            ),
+                          ),
                         ),
-                        const SizedBox(width: 20),
-                        switch (state.type) {
-                          DownloadTaskStateType.idle => GestureDetector(
-                              behavior: HitTestBehavior.opaque,
-                              onTap: () {
-                                ref.read(provider.notifier).doDownload();
-                              },
-                              child: const Icon(Icons.download_outlined),
-                            ),
-                          DownloadTaskStateType.busy => const CupertinoActivityIndicator(),
-                          DownloadTaskStateType.completed => GestureDetector(
-                              behavior: HitTestBehavior.opaque,
-                              onTap: () {
-                                ref.read(provider.notifier).doDownload(retry: true);
-                              },
-                              child: const Icon(Icons.check_outlined),
-                            ),
-                          DownloadTaskStateType.failed => GestureDetector(
-                              behavior: HitTestBehavior.opaque,
-                              onTap: () {
-                                ref.read(provider.notifier).doDownload(retry: true);
-                              },
-                              child: const Icon(Icons.error_outline),
-                            ),
-                        }
-                      ],
-                    ),
-                    const SizedBox(height: 10),
-                    LinearProgressIndicator(value: state.progress),
-                  ],
+                      )
+                    ],
+                  ),
                 ),
               ),
             ],
