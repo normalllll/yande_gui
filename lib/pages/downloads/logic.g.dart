b8da5cd1b282780ddfd7d447adac2aa789e45626
normalllll <147307373+normalllll@users.noreply.github.com>
2024-12-30 18:22:15 +0100

    Update dependencies

--- lib/pages/downloads/logic.g.dart
+++ lib/pages/downloads/logic.g.dart
@@ -170,6 +170,8 @@ class DownloadTaskProvider
   }
 }
 
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
 mixin DownloadTaskRef on NotifierProviderRef<DownloadTaskState> {
   /// The parameter `post` of this provider.
   Post get post;
@@ -184,4 +186,4 @@ class _DownloadTaskProviderElement
   Post get post => (origin as DownloadTaskProvider).post;
 }
 // ignore_for_file: type=lint
-// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
--- lib/pages/post_detail/logic.g.dart
+++ lib/pages/post_detail/logic.g.dart
@@ -142,6 +142,8 @@ class GetSimilarProvider extends AutoDisposeFutureProvider<Similar> {
   }
 }
 
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
 mixin GetSimilarRef on AutoDisposeFutureProviderRef<Similar> {
   /// The parameter `id` of this provider.
   int get id;
@@ -155,4 +157,4 @@ class _GetSimilarProviderElement
   int get id => (origin as GetSimilarProvider).id;
 }
 // ignore_for_file: type=lint
-// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
