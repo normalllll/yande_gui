1dde9b182df691c0b9bbb5bbcb1d7fe74f8d0b38
normalllll <147307373+normalllll@users.noreply.github.com>
2025-04-12 23:09:47 +0200

    Fix RefreshControl

--- lib/pages/post_list/post_list_page.dart
+++ lib/pages/post_list/post_list_page.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -25,8 +27,61 @@ class PostListPage extends ConsumerStatefulWidget {
   ConsumerState createState() => _PostListPageState();
 }
 
+const double _kActivityIndicatorRadius = 14.0;
+
 class _PostListPageState extends ConsumerState<PostListPage> {
   late final provider = postListProvider(runtimeType, tags: widget.tags ?? []);
+
+  bool get needTopPadding => widget.tags == null;
+
+   Widget buildRefreshIndicator(
+    BuildContext context,
+    RefreshIndicatorMode refreshState,
+    double pulledExtent,
+    double refreshTriggerPullDistance,
+    double refreshIndicatorExtent,
+  ) {
+    final double percentageComplete = clampDouble(pulledExtent / refreshTriggerPullDistance, 0.0, 1.0);
+
+    // Place the indicator at the top of the sliver that opens up. We're using a
+    // Stack/Positioned widget because the CupertinoActivityIndicator does some
+    // internal translations based on the current size (which grows as the user drags)
+    // that makes Padding calculations difficult. Rather than be reliant on the
+    // internal implementation of the activity indicator, the Positioned widget allows
+    // us to be explicit where the widget gets placed. The indicator should appear
+    // over the top of the dragged widget, hence the use of Clip.none.
+
+    return Center(
+      child: Padding(
+        padding: EdgeInsets.only(top: needTopPadding ? MediaQuery.of(context).padding.top + 4 : 0),
+        child: _buildIndicatorForRefreshState(refreshState, _kActivityIndicatorRadius, percentageComplete),
+      ),
+    );
+  }
+
+  static Widget _buildIndicatorForRefreshState(RefreshIndicatorMode refreshState, double radius, double percentageComplete) {
+    switch (refreshState) {
+      case RefreshIndicatorMode.drag:
+        // While we're dragging, we draw individual ticks of the spinner while simultaneously
+        // easing the opacity in. The opacity curve values here were derived using
+        // Xcode through inspecting a native app running on iOS 13.5.
+        const Curve opacityCurve = Interval(0.0, 0.35, curve: Curves.easeInOut);
+        return Opacity(
+          opacity: opacityCurve.transform(percentageComplete),
+          child: CupertinoActivityIndicator.partiallyRevealed(radius: radius, progress: percentageComplete),
+        );
+      case RefreshIndicatorMode.armed:
+      case RefreshIndicatorMode.refresh:
+        // Once we're armed or performing the refresh, we just show the normal spinner.
+        return CupertinoActivityIndicator(radius: radius);
+      case RefreshIndicatorMode.done:
+        // When the user lets go, the standard transition is to shrink the spinner.
+        return CupertinoActivityIndicator(radius: radius * percentageComplete);
+      case RefreshIndicatorMode.inactive:
+        // Anything else doesn't show anything.
+        return const SizedBox.shrink();
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -43,23 +98,30 @@ class _PostListPageState extends ConsumerState<PostListPage> {
         return LoadingMoreCustomScrollView(
           physics: const BouncingScrollPhysics(parent: AlwaysScrollableScrollPhysics()),
           slivers: [
-            SliverPadding(padding: EdgeInsets.only(top: MediaQuery.paddingOf(context).top)),
             if (widget.tags != null)
               SliverAppBar(
                 floating: isMobile || widget.tags == null,
                 snap: isMobile,
                 pinned: isDesktop && widget.tags != null,
                 scrolledUnderElevation: 0,
-                title: switch (widget.tags) { final tags? => Text(i18n.postList.titleWithTags(tags.join(' '))), _ => Text(i18n.postList.title) },
+                title: switch (widget.tags) {
+                  final tags? => Text(i18n.postList.titleWithTags(tags.join(' '))),
+                  _ => Text(i18n.postList.title),
+                },
               ),
             CupertinoSliverRefreshControl(
               onRefresh: () async {
                 await state.source.refresh(false, false);
               },
+              refreshIndicatorExtent: 25,
+              builder: buildRefreshIndicator,
             ),
+            if (needTopPadding) SliverToBoxAdapter(child: SizedBox(height: MediaQuery.of(context).padding.top)),
             LoadingMoreSliverList(
               SliverListConfig(
-                extendedListDelegate: SliverWaterfallFlowDelegateWithFixedCrossAxisCount(crossAxisCount: SettingsService.waterfallColumns ?? columnsMax),
+                extendedListDelegate: SliverWaterfallFlowDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: SettingsService.waterfallColumns ?? columnsMax,
+                ),
                 itemBuilder: (BuildContext context, item, int index) {
                   return GestureDetector(
                     onTap: () {
@@ -86,10 +148,7 @@ class _PostListPageState extends ConsumerState<PostListPage> {
                                   width: width,
                                   height: height,
                                   imageBuilder: (child) {
-                                    return Hero(
-                                      tag: item.id,
-                                      child: child,
-                                    ).animate().fade(duration: 250.ms);
+                                    return Hero(tag: item.id, child: child).animate().fade(duration: 250.ms);
                                   },
                                 ),
                               ),
@@ -143,27 +202,29 @@ class _PostListPageState extends ConsumerState<PostListPage> {
                 },
                 sourceList: state.source,
                 padding: EdgeInsets.zero,
-                indicatorBuilder: (context, status) => LoadingMoreIndicator(
-                  status: status,
-                  isSliver: true,
-                  errorRefresh: () {
-                    state.source.errorRefresh();
-                  },
-                ),
+                indicatorBuilder:
+                    (context, status) => LoadingMoreIndicator(
+                      status: status,
+                      isSliver: true,
+                      errorRefresh: () {
+                        state.source.errorRefresh();
+                      },
+                    ),
               ),
             ),
           ],
         );
       },
-      floatingActionButton: isDesktop
-          ? FloatingActionButton(
-              heroTag: '${runtimeType}FloatingActionButton',
-              onPressed: () {
-                state.source.refresh(true);
-              },
-              child: const Icon(Icons.refresh, color: Colors.white),
-            )
-          : null,
+      floatingActionButton:
+          isDesktop
+              ? FloatingActionButton(
+                heroTag: '${runtimeType}FloatingActionButton',
+                onPressed: () {
+                  state.source.refresh(true);
+                },
+                child: const Icon(Icons.refresh, color: Colors.white),
+              )
+              : null,
     );
   }
 }
