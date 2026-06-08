1d0322d533f1f180b7ed87b8c728b993b0023472
normalllll <147307373+normalllll@users.noreply.github.com>
2026-06-08 20:28:44 +0800

    feat: update window title

--- linux/my_application.cc
+++ linux/my_application.cc
@@ -40,11 +40,11 @@ static void my_application_activate(GApplication* application) {
   if (use_header_bar) {
     GtkHeaderBar* header_bar = GTK_HEADER_BAR(gtk_header_bar_new());
     gtk_widget_show(GTK_WIDGET(header_bar));
-    gtk_header_bar_set_title(header_bar, "yande_gui");
+    gtk_header_bar_set_title(header_bar, "Yande GUI");
     gtk_header_bar_set_show_close_button(header_bar, TRUE);
     gtk_window_set_titlebar(window, GTK_WIDGET(header_bar));
   } else {
-    gtk_window_set_title(window, "yande_gui");
+    gtk_window_set_title(window, "Yande GUI");
   }
 
   gtk_window_set_default_size(window, 1280, 720);
